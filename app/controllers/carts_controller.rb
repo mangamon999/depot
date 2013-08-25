9b910ba4f93398c3131b3c49e98294ae54ef6c3c
mangamon999 <mangamon999@gmail.com>
2013-08-25 20:51:37 -0300

    Implemented error handling for invalid cart ids
    If the user manualy type an invalid cart id in the url, he will be
    redirected to the catalog and a message will say "Invalid cart"

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -62,13 +62,24 @@ class CartsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_cart
-      @cart = Cart.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_cart
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def cart_params
-      params[:cart]
+    begin
+      @cart = Cart.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      redirect_to store_url, notice: 'Invalid cart'
+    else
+      respond_to do |format|
+        format.html #show html.erb
+        format.json {render json: @cart}
+      end
     end
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def cart_params
+    params[:cart]
+  end
 end
